e3bba96daf3c741bb7c2e1f74a5159b49cfc2d88
Taryn Martin <taryn.martin919@gmail.com>
2020-10-15 13:42:05 -0600

    Add action file for Survey file

---
+++ code/web/src/modules/survey/api/actions.js
@@ -0,0 +1,6 @@
+// create action function to post user style type
+  // create loading dispatch action
+  // create axios post request; push up user array of values from images they chose to update database
+  // if successful response, add user style type to redux store/local storage? Update user in store through user/api/state.js file
+  // redirect to crate sign up to continue w/registering for a crate w/a style type
+  // do error/catch handling
