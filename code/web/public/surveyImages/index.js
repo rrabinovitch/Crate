388c8f634dcb0c453193fbca6f38b61f2475d7b1
Taryn Martin <taryn.martin919@gmail.com>
2020-10-15 16:52:49 -0600

    Add folders to store images for survey

---
+++ code/web/public/surveyImages/index.js
@@ -0,0 +1,11 @@
+// sporty = 1
+// scary = 2
+// posh = 3
+// ginger = 4
+// baby = 5
+import { APP_URL } from '../../setup/config/env'
+
+export const topArray = [`${APP_URL}/surveyImages/sporty/image`, `${APP_URL}/surveyImages/scary/scaryTop.jpg`, `${APP_URL}/surveyImages/posh/image`, `${APP_URL}/surveyImages/ginger/image`, `${APP_URL}/surveyImages/baby/image`];
+export const bottomArray = [];
+export const shoesArray = [];
+export const accessoriesArray = [];
