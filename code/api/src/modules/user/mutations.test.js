2251b8b332b619e89be64500ff92fae63a5dc6b2
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-10-17 15:38:22 -0600

    Set up user mutations test

---
+++ code/api/src/modules/user/mutations.test.js
@@ -0,0 +1,40 @@
+import request from 'supertest';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from '../../setup/schema';
+import models from '../../setup/models';
+
+const bcrypt = require('bcrypt');
+
+describe("user mutations", () => {
+  let server;
+
+  beforeAll(() => {
+    server = express(); // setting up server
+
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false
+      })
+    );
+  });
+
+  beforeAll(async () => {
+    const userData1 = {
+      id: 1,
+      name: "The User",
+      email: "user@crate.com",
+      password: bcrypt.hashSync('password321', 10),
+      role: "user"
+    };
+
+    await models.User.create(userData1);
+  });
+
+  afterAll(async () => {
+    await models.User.destroy({ where: {} })
+    server.close();
+  });
+})
